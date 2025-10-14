ef2fe9e200abdc816d6c9dcdd352b58eaabf17b2
nguyen-thanh-huy1204 <102240193@sv1.dut.udn.vn>
2025-10-14 21:08:39 +0700

    Update student.h

--- student.h
+++ student.h
@@ -2,22 +2,34 @@
 #define STUDENT_H
 
 #include "User.h"
+#include "Course.h"
+#include <vector>
 #include <string>
 using namespace std;
 
-class Student : public User {
+class Student: public User {
 private:
-    string id;
-    string hoTen;
-    int namHoc;
-    string nganhHoc;
+    string id;                    
+    string hoTen;                
+    int namHoc;                    
+    vector<Course> danhSachDangKy;  
 
 public:
     Student(const string& tenDN, const string& matKhau,
             const string& id, const string& hoTen,
-            int namHoc, const string& nganhHoc);
+            int namHoc);
 
     void Menu() override;
+
+    void dangKyHocPhan();
+
+    void xemHocPhanDaDangKy() const;
+
+    void huyHocPhan(const string& idHP);
+
+    void luuVaoFile() const;
+
+    void taiTuFile();
 };
 
 #endif
