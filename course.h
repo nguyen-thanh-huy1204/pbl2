3c6fa9bb69045be0d6eda7446f59d8a2ad29c184
nguyen-thanh-huy1204 <102240193@sv1.dut.udn.vn>
2025-10-14 21:07:33 +0700

    Update course.h

--- course.h
+++ course.h
@@ -7,19 +7,20 @@ using namespace std;
 
 class Course {
 private:
-    string courseID;
-    string courseName;
-    int credits;
+    string courseID;        
+    string courseName;         
+    int creditCourse;               
+    string ID_mon_tien_quyet;   
 
 public:
-    Course(const string& id = "", const string& name = "", int cr = 0);
-
-    string getCourseID() const { return courseID; }
-    string getCourseName() const { return courseName; }
-    int getCredits() const { return credits; }
+    Course(const string& id = "", const string& name = "", int credits = 0, const string& mon_tien_quyet = "");
+    
+    string getCourseID() const;
+    string getCourseName() const;
+    int getCreditCount() const;
+    string getPrerequisiteCourseID() const;
 
     void display() const;
-
     void saveToFile(ostream& out) const;
     bool loadFromFile(istream& in);
 };
